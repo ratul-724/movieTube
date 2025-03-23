dd3f835b3c244d6d25e2556121441c170f102f8e
rhratul306@gmail.com <RaTuL HasAn>
2025-03-23 18:10:40 +0600

    movie tube

---
+++ js/common.js
@@ -0,0 +1,9 @@
+// Toggle Search Bar
+function toggleSearch() {
+    const searchBar = document.getElementById('searchBar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
+    }
+}
