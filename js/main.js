1ad057f095a16674977a05b1e8bbb6adbb3d2ebd
rhratul306@gmail.com <rhratul306@gmail.com>
2025-03-27 00:55:34 +0600

    db connect

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
-
+// code for counter start 
 document.addEventListener('DOMContentLoaded', function() {
-const counters = document.querySelectorAll('.count');
-const speed = 200; // The lower the faster
-
-counters.forEach(counter => {
-    const target = +counter.getAttribute('data-target');
-    const count = +counter.innerText;
-    const increment = target / speed;
+    const counters = document.querySelectorAll('.count');
+    const speed = 200; // The lower the faster
     
-    const updateCount = () => {
-    const current = +counter.innerText;
-    
-    if(current < target) {
-        counter.innerText = Math.ceil(current + increment);
-        setTimeout(updateCount, 1);
-    } else {
-        counter.innerText = target;
-    }
-    };
-    
-    // Start counting when element is in viewport
-    const observer = new IntersectionObserver((entries) => {
-    if(entries[0].isIntersecting) {
-        updateCount();
-        observer.unobserve(counter);
-    }
+    counters.forEach(counter => {
+        const target = +counter.getAttribute('data-target');
+        // const count = +counter.innerText;
+        const increment = target / speed;
+        const updateCount = () => {
+        const current = +counter.innerText;
+        
+        if(current < target) {
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+        };
+        
+        // Start counting when element is in viewport
+        const observer = new IntersectionObserver((entries) => {
+        if(entries[0].isIntersecting) {
+            updateCount();
+            observer.unobserve(counter);
+        }
+        });
+        
+        observer.observe(counter);
     });
-    
-    observer.observe(counter);
 });
-});
+// code for counter end 
 
 // for favorite button code satrt here 
-// Favorite button functionality
 document.querySelectorAll('.favorite-btn').forEach(btn => {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
